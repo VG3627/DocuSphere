5836cac59174cc97791157701b61d79791fca225
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-07-05 19:04:39 +0530

    update

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -376,7 +376,7 @@ const handleRemoveAuthor = async (userMail) => {
               {isDropdownOpen && (
                 <div className="absolute mt-2 w-50 bg-white shadow-lg border border-gray-300 rounded">
                   {authors && authors.length > 0 ? (
-                    authors.map(({ userMail, permission}) => (
+                    authors.map(({ userMail, role : permission}) => (
                       <div key={userMail} className="flex justify-between items-center p-2 border-b border-gray-10">
                         {<span>
                           {userMail} - {permission}
